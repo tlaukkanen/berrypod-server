2f9737c4cad0e61b27d86074900693a91282f322
Konsta Laakso <konsta.laakso@gmail.com>
2016-10-12 19:29:24 +0300

    Add handlers for testing purposes, Joi for validation, initial planning of endpoints

---
+++ handlers/handlers.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  // Generic handlers
+  getServerState: (req, res) => {
+    return res('The server is up and running');
+  },
+
+  notYetImplemented: (req, res) => {
+    return res('Not yet implemented, but thanks for trying!');
+  },
+
+  // Profile
+  getProfile: (req, res) => {
+    return res({
+      userid: '1',
+      username: 'Anonymous',
+      firstname: 'Dread Pirate',
+      lastname: 'Roberts'
+    })
+  }
+};
