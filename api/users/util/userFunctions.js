83698b9dcb1f96812c4263ebd1ff2013627ddcc2
Konsta Laakso <konsta.laakso@gmail.com>
2016-10-19 08:40:21 +0300

    Refactor project structure, add RethinkDB driver & JWT authentication strategy

---
+++ api/users/util/userFunctions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Boom = require('boom');
+const User = require('../model/User');
+
+function verifyUniqueUser(req, res) {
+
+}
+
+module.exports = {
+  verifyUniqueUser: verifyUniqueUser
+}
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict';
 
-const Hapi = require('hapi');
-const Fs = require('fs');
-const Nes = require('nes');
-const Inert = require('inert');
-const Vision = require('vision');
-const HapiSwagger = require('hapi-swagger');
-const Pack = require('./package');
+// ------------------------------------------------------------------------------------- //
+const Hapi = require('hapi');                     // Hapi.js Framework
+const Nes = require('nes');                       // Websocket adapter for Hapi
+const Inert = require('inert');                   // Static file and directory handler
+const Vision = require('vision');                 // Template rendering plugin
+const HapiSwagger = require('hapi-swagger');      // Swagger interface for Hapi
+const HapiAuthJwt = require('hapi-auth-jwt');     // JWT authentication plugin for Hapi
 
-const routes = require('./config/routes');
+const rethink = require('rethinkdb');             // RethinkDB driver
+const glob = require('glob');
+const path = require('path');
+const Pack = require('./package');
+// ------------------------------------------------------------------------------------- //
+
+// Create Hapi server instance
 const server = new Hapi.Server();
 
 server.connection({
@@ -22,20 +28,49 @@ const swaggerOptions = {
 };
 
 server.register([
+  HapiAuthJwt,
   Nes,
   Inert,
   Vision,
-  {
-    'register': HapiSwagger,
-    'options': swaggerOptions
-  }], (err) => {
-    server.start( (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Server running at:', server.info.uri);
-      }
-    });
+  { 'register': HapiSwagger, 'options': swaggerOptions }
+], (err) => {
+
+  // Add JWT as authentication strategy
+  server.auth.strategy('jwt', 'jwt', {
+    key: 'insertVerySercretKeyHere',
+    verifyOptions: {
+      algorithms: ['HS256']
+    }
   });
 
-server.route(routes);
+  // Look through the routes in
+  // all the subdirectories of API
+  // and create a new route for each
+  glob.sync('api/**/routes/*.js', {
+    root: __dirname
+  }).forEach(file => {
+    const route = require(path.join(__dirname, file));
+    server.route(route);
+  });
+
+  // Start the server
+  server.start((err) => {
+    if (err) throw err;
+
+    console.log('Server running at:', server.info.uri);
+    console.log('Connecting to rethinkdb...');
+
+    rethink.connect({ host: 'localhost', port: 28015 }, (err, conn) => {
+      if (err) throw err;
+      console.log('Successfully connected to RethinkDB @', conn.host + ':' + conn.port);
+
+      /*
+      rethink.db('test').tableCreate('tv_shows').run(conn, (err, res) => {
+        if (err) throw err;
+        console.log(res);
+      });
+      */
+    });
+
+  });
+});
